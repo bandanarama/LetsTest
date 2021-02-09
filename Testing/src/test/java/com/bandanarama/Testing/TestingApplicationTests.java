e5caad518e62c4b1bc673679488696abc73cf2f7
bandanarama <kvoon0@frostburg.edu>
2021-02-08 20:31:15 -0700

    first commit, initial project structure

---
+++ Testing/src/test/java/com/bandanarama/Testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandanarama.Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
